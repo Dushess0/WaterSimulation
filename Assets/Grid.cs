f01e79b65c339f173227ab332e2cd3f475154693
Dushess0 <ilyabannila@gmail.com>
2024-01-25 13:51:50 +0100

    change back to stone

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -90,7 +90,7 @@ namespace WaterSimulation
                         // add a border
                         if (y == Height - 1)
                         {
-                            cell.SetType(CellType.Solid,CellStyle.Grass);
+                            cell.SetType(CellType.Solid,CellStyle.Stone);
                         }
 
                         cell.transform.parent = cellContainer.transform;
